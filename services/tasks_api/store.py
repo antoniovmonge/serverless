c10e4cc9a33957e8015b78b297cf8c936a287ecf
Antonio Vilchez <antoniovmonge@gmail.com>
2023-10-29 15:40:34 +0100

    Add Listing Tasks

--- services/tasks_api/store.py
+++ services/tasks_api/store.py
@@ -1,7 +1,10 @@
+import datetime
+
 from uuid import UUID
 
 import boto3
 
+from boto3.dynamodb.conditions import Key
 from models import Task, TaskStatus
 
 
@@ -38,3 +41,51 @@ class TaskStore:
             owner=record["Item"]["owner"],
             status=TaskStatus[record["Item"]["status"]],
         )
+
+    def add(self, task):
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table(self.table_name)
+        table.put_item(
+            Item={
+                "PK": f"#{task.owner}",
+                "SK": f"#{task.id}",
+                "GS1PK": f"#{task.owner}#{task.status.value}",
+                "GS1SK": f"#{datetime.datetime.utcnow().isoformat()}",
+                "id": str(task.id),
+                "title": task.title,
+                "status": task.status.value,
+                "owner": task.owner,
+            }
+        )
+
+    def list_open(self, owner):
+        dynamodb = boto3.resource("dynamodb")
+        table = dynamodb.Table(self.table_name)
+        last_key = None
+        query_kwargs = {
+            "IndexName": "GS1",
+            "KeyConditionExpression": Key("GS1PK").eq(
+                f"#{owner}#{TaskStatus.OPEN.value}"
+            ),
+        }
+        tasks = []
+        while True:
+            if last_key is not None:
+                query_kwargs["ExclusiveStartKey"] = last_key
+            response = table.query(**query_kwargs)
+            tasks.extend(
+                [
+                    Task(
+                        id=UUID(record["id"]),
+                        title=record["title"],
+                        owner=record["owner"],
+                        status=TaskStatus[record["status"]],
+                    )
+                    for record in response["Items"]
+                ]
+            )
+            last_key = response.get("LastEvaluatedKey")
+            if last_key is None:
+                break
+
+        return tasks
